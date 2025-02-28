1c0c6698c4143e6bbe612496866c60ffdb319b6f
HAFIDH AHMAD FAUZAN <hafidhfauzan2@gmail.com>
2025-02-28 14:35:05 +0700

    refactor: boolean output table

--- shared/models/role/layout/table.tsx
+++ shared/models/role/layout/table.tsx
@@ -3,6 +3,7 @@ import { LayoutTable } from "system/model/layout/types";
 export const table: LayoutTable<"Role"> = {
   columns: [
     { col: "name" },
-    { rel: "user", col: "username" }
+    { col: "active" },
+    { rel: "user", col: "username" },
   ],
 };
